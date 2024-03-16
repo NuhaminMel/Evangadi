281fc665d1ff9fdeda3ffeb655eb8ca6e1f46f20
Saleamlak21 <fentaneshmesfin@gmail.com>
2024-03-16 08:53:04 -0400

    reinstall the next app

---
+++ Frontend/evangadi/public/assets/index.tsx
@@ -0,0 +1,66 @@
+import manOne from './3-steps12.jpg'
+import manTwo from './3-steps9.jpg'
+import manThree from './immersive4.jpg'
+import womanOne from './immersive7.jpg'
+import fiveSteps from './5-step-week-white3.jpg'
+import aprilBatch from './AprilBatch5.jpg'
+import asche from './AscheThumb2.jpg'
+import khalid from './KhalidThumb.jpg'
+import niko from './NikoThumb.jpg'
+import yemsrach from './YemsrachThumb.jpg'
+import aws from './aws2.png'
+import bgTech13 from './bg1-evangadiTech13.jpg'
+import bgTech19 from './bg1-evangadiTech19.jpg'
+import bgTech23 from './bg1-evangadiTech23.jpg'
+import bi from './bi.png'
+import git from './git.png'
+import hammerKid3 from './hammer-kid3.jpg'
+import learnTwo from './learn2.jpg'
+import learnThree from './learn3.jpg'
+import learnFour from './learn4.jpg'
+import mulesoft from './mulesoft.png'
+import sharepoint from './sharepoint.png'
+import soft from './soft.jpg'
+import sql from './sql.png'
+import team from './team.jpg'
+import pm from './pm.jpg'
+import qa from './qa.png'
+import review from './review.jpg'
+import standard from './standard.jpg'
+import student from './student-in-class4.jpg'
+
+
+const Image = {
+  manOne,
+  manTwo,
+  manThree,
+  womanOne,
+  fiveSteps,
+  aprilBatch,
+  asche,
+  khalid,
+  niko,
+  yemsrach,
+  aws,
+  bgTech13,
+  bgTech19,
+  bgTech23,
+  bi,
+  git,
+  hammerKid3,
+  learnTwo,
+  learnThree,
+  learnFour,
+  mulesoft,
+  pm,
+  qa,
+  review,
+  sharepoint,
+  soft,
+  sql,
+  team,
+  standard,
+  student
+};
+
+export default Image
